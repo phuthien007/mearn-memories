b6bf3bca046d6fe189c44dd54fcc24d0c1c131cb
phuthien007 <phailamsaonana@gmail.com>
2022-03-18 09:21:08 +0700

    add deploy

--- src/api/config.js
+++ src/api/config.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-const url = 'http://localhost:5000/api/v1'
+const url = 'https://memories-project-git.herokuapp.com/api/v1'
 
 const config = {
     get: (uri) => axios.get(`${url}/${uri}`),
-    post: (uri, payload) => axios.post(`${url}/${uri}`,payload),
-    update: (uri,id, payload) => axios.patch(`${url}/${uri}/${id}`,payload),
+    post: (uri, payload) => axios.post(`${url}/${uri}`, payload),
+    update: (uri, id, payload) => axios.patch(`${url}/${uri}/${id}`, payload),
     delete: (uri, id) => axios.delete(`${url}/${uri}/${id}`),
     increase: (uri, id) => axios.put(`${url}/${uri}/${id}`)
 }
